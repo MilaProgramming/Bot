f5aaa73edfda535c5c79f7ebe9037c88efbcbb1e
Mila <carivera14@espe.edu.ec>
2023-08-04 03:14:18 -0500

    json consumible

--- src/alimentos.js
+++ src/alimentos.js
@@ -1,35 +1,29 @@
 const {REST, Routes, ApplicationCommandOptionType} = require('discord.js');
-const Realm = require('realm');
 require('dotenv').config();
+  
 
 const commands = [
     {
         name: 'alimentos',
         description: 'Muestra los alimentos en la BD',
-        options: [
-            {
-                name: 'nombre-alimento',
-                description: 'Nombre del alimento',
-                type: ApplicationCommandOptionType.STRING,
-            }
-        ]
+        // options: [
+        //     {
+        //         name: 'nombre-alimento',
+        //         description: 'Nombre del alimento',
+        //         type: ApplicationCommandOptionType.String,
+        //         required: true,
+        //     }
+        // ]
     },
 ]
 
 const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
-(async () =>{
+(async () => {
 
     try{
-
-        const app = new Realm.App({ id: "botdiscord-pxxiu" });
-        const credentials = Realm.Credentials.anonymous();
-        try {
-        const user = await app.logIn(credentials);
-        } catch(err) {
-        console.error("Failed to log in", err);
-        }
-
+        
+      
         console.log("Inicio el proceso");
         
         await rest.put(
@@ -37,7 +31,10 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
             {body: commands},
         );
 
+
         console.log("Termino el proceso");
+
+        
 
     }catch (error){
         console.log(`Hubo un error: ${error}`);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const {Client, GatewayIntentBits} = require('discord.js');
 require('dotenv').config();
+const axios = require('axios');
+
+const prefix = '!';
+
 //Instents es un set de permisos para dar acceso a sets de eventos
 
 /* El código está creando una nueva instancia de la clase `Cliente` de la biblioteca `discord.js`. La
@@ -27,10 +31,41 @@ pollito.on('messageCreate', (message) => {
     }
 });
 
-pollito.on('interactionCreate', (interaction) => {
+pollito.on('interactionCreate', async interaction => {
 
     if (!interaction.isChatInputCommand()) return;
 
-    const nombre = interaction.options.get('nombre-alimento')?.value;
-  
+
+    if(interaction.commandName === 'alimentos'){ 
+
+        let getAlimentos = async () => {
+
+        let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: 'https://us-east-1.aws.data.mongodb-api.com/app/botdiscord-pxxiu/endpoint/pollitoAlimentos',
+                headers: { 
+                  'api-key': 'WS13QVMfhcM5TqLvkdIev4IfD50p8aFMIvOayeMiQQg0xONpuNkJWqnmyO4LRPrn'
+                }
+        };
+            
+        let response =     axios.request(config)
+                .then((response) => {
+                console.log(JSON.stringify(response.data));
+                })
+                .catch((error) => {
+                console.log(error);
+                });
+
+        let result = response.data;
+
+        return result;
+
+        }
+
+        let valor = await getAlimentos();
+        console.log(valor);
+
+
+    }
 });
